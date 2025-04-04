1829448ba6e69a01b37625333c7fe01df29528f2
fastroc <nyamjavbataa@gmail.com>
2025-04-04 14:11:22 +0000

    Fix 500 error by importing checkPaymentStatus in routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createInvoice } = require('../services/paymentService');
+const { createInvoice, checkPaymentStatus, pool } = require('../services/paymentService');
 const winston = require('winston');
 
 const logger = winston.createLogger({
